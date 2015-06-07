602d8b8d5b67c0bf41063a71a9eb11713e6fa4ed
Armin <armin@git.arminius.org>
2015-06-07 02:13:19 +0200

    bugfixes

--- install.sh
+++ install.sh
@@ -7,9 +7,12 @@
 # This file is part of mescaline for zsh.
 #
 
+# exit on errors
+set -e
+
 # parameter expansion to extract the scripts home directory
-script="${0%/*}"
-home="$(readlink -f $script)"
+home="${0%/*}"
+# cd to $home
 cd "$home"
 
 # print a message
@@ -24,7 +27,7 @@ fileexists () {
   if [ -e "$file" ]; then
     msg "File or directory exists: "
     msg "${file}"
-    msg "I'm not going to overwrite it, so you have to resolve this."
+    msg "I'm not going to overwrite it, so you have to resolve this manually."
     msg "Exiting gracefully. Goodbye."
     exit 1
   fi
@@ -37,7 +40,7 @@ fileexists "$HOME/.zshrc"
 mkdir -p "$HOME/.mescaline"
 cp -R ./ "$HOME/.mescaline"
 cd
-ln -s .mescaline/zshrc .zshrc
+ln -s .mescaline/zshrc .zshrc || msg 'Symbolic link ~/.zshrc exists. Delete this manually.'
 
 msg "setup complete."
 
